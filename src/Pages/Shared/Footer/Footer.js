d87ed6400dbbc6dbbedcc4197b132e900d2f99c3
irfanhsajid <irfanhsajid@gmail.com>
2023-10-22 09:16:22 +0600

    vercel config file added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,8 +1,6 @@
 import React from 'react';
 const Footer = () => {
     return (
-
-
         <div className="py-3">
             <p className="p2 text-center text-muted fs-6 ">
                 <small>&copy; Irfan H Sajid 2023  <span className="text-danger"> | | </span> All Rights Reserved
